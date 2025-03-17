f7ba4fa47d74292422f668499bbe4d684d5b6920
AnastasiaYakovleva04 <nastasiakovleva@gmail.com>
2025-03-17 11:10:17 +0500

    Student.kt

--- Student.kt
+++ Student.kt
@@ -1,88 +1,102 @@
 import java.time.LocalDate
 import java.time.Period
 import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
 
-open class Student() {
-
-    var name: String = "" //имя
-    var surname: String = "" //фамилия
-    var patronimic: String = "" //отчество
-    var sex: Char = '?' //пол
-    var group: String = "" //группа
-    var bday: LocalDate = LocalDate.now() //дата рождения
-    var height: Double = 0.0 //рост
-    var weigth: Double = 0.0 //вес
-    var sport: String = "" //спорт
-    var budget: Boolean = false //бюджет-внебюджет
-
-
-    //Заполнение информации о студенте
-    open fun Input(){
+open class Student(var name: String = "",
+                   var surname: String = "",
+                   var patronimic: String = "",
+                   var sex: Char = '?',
+                   var group: String = "",
+                   var bday: LocalDate = LocalDate.now(),
+                   var height: Double = 0.0,
+                   var weigth: Double = 0.0,
+                   var sport: String = "",
+                   var budget: Boolean = false) {
+    var correct = true
+   open fun Input(){
         try {
-            val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+            val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
 
             print("Введите фамилию: ")
             surname = readln()
-
             print("Введите имя: ")
             name = readln()
-
             print("Введите отчество: ")
             patronimic = readln()
-
             print("Введите пол: ")
             val ch = readln()
             if (ch.length == 1)
                 sex = ch[0]
-
-            print("Введите дату рождения (гггг-мм-дд): ")
+            else
+                correct = false
+            print("Введите группу: ")
+            group = readln()
+            print("Введите дату рождения (дд.мм.гггг): ")
             val bdate = readln()
-            bday = LocalDate.parse(bdate, formatter)
-
+            bday = try {
+                LocalDate.parse(bdate, formatter)
+            }
+            catch (e: DateTimeParseException)
+            {
+                correct = false
+                println("Неверный формат даты")
+                LocalDate.now()
+            }
             print("Введите рост: ")
             height = readln().toDouble()
-
             print("Введите вес: ")
             weigth = readln().toDouble()
-
             print("Введите вид спорта: ")
             sport = readln()
-
-            print("Введите вид обучения (бюджет/внебюджет): ")
-            val bgt = readln()
-            when (bgt) {
+            CheckCorrect(name, surname, patronimic, sex, group, bday, height, weigth, sport)
+            print("Введите бюджет или внебюджет: ")
+            val bdgt = readln()
+            when (bdgt) {
                 "бюджет" -> budget = true
                 "внебюджет" -> budget = false
                 else -> {
-                    println("Некорректная запись")
+                    correct = false
+                    println("Неверный ввод")
                     return
                 }
             }
-
-            if (!CheckCorrect())
-                println("Некорректная запись")
         }
         catch (e:Exception)
         {
             println("Неверный формат данных")
+            correct = false
         }
     }
 
     //Проверка корректности ввода
-    fun CheckCorrect ():Boolean{
-
+    fun CheckCorrect (name: String, surname: String, patronimic: String,
+                      sex: Char, group: String, bday: LocalDate, height: Double, weigth: Double, sport: String):String{
+        try {
             if (name != "" && surname != "" && patronimic != "" && (sex == 'м' || sex == 'ж') && group != ""
-                && bday != LocalDate.now() && height > 140.0 && weigth > 40.0 && sport != "")
-                return true
+                && bday != LocalDate.now() && height > 140.0 && height < 210.0 && weigth > 40.0 && sport != "") {
+                correct = false
+                return "Некорректная запись данных"
+            }
             else
-                return false
+                return "Корректная запись данных"
+        }
+        catch (e:Exception)
+        {
+            return "Неверный формат данных"
+        }
     }
 
     //Вывод информации о студенте
-    fun Info(): String {
-            return "Студент $surname $name $patronimic, рожден $bday, обучается в группе $group, занимается $sport. Рост: $height, вес: $weigth."
-                // else -> return "Студентка $surname $name $patronimic, рождена $bday, обучается в группе $group, занимается $sport. Рост: $height, вес: $weigth."
-
+    open fun Info(): String {
+        if (correct) {
+            when (sex) {
+                'м' -> return "Студент $surname $name $patronimic, рожден $bday, обучается в группе $group, занимается $sport. Рост: $height, вес: $weigth."
+                'ж' -> return "Студентка $surname $name $patronimic, рождена $bday, обучается в группе $group, занимается $sport. Рост: $height, вес: $weigth."
+                else -> return "Некорректная запись"
+            }
+        }
+        else return "Информация внесена некорректно"
     }
 
     //Вычисление возраста студента
@@ -94,13 +108,7 @@ open class Student() {
 
     //Вычисление индекса массы тела
     fun IMT ():Double {
-        var imt: Double = 0.0
-        if (height < 2.5)
-            imt = weigth/height
-        else
-            imt = weigth/(height/100)
+        val imt = weigth/(Math.pow(height/100, 2.0))
         return imt
     }
-
-
 }

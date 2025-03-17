2727deac7ffd3f1235945ef1ed5075ed77d5f620
AnastasiaYakovleva04 <nastasiakovleva@gmail.com>
2025-03-17 11:11:00 +0500

    Budget.kt

--- Budget.kt
+++ Budget.kt
@@ -1,32 +1,40 @@
-class Budget: Student() {
+class Budget: Student(budget = true) {
     var scholarship: Double = 0.0 //стипендия
     var count5 : UInt = 0u //количество 5
     var count4 : UInt = 0u //количество 4
     var count3 : UInt = 0u //количество 3
 
     //Перегрузка функции ввода информации
-    override fun Input() {
+    override fun Input(){
         try {
-            print("Введите количество 5:")
+            print("Введите количество 5: ")
             count5 = readln().toUInt()
-            print("Введите количество 4:")
+            print("Введите количество 4: ")
             count4 = readln().toUInt()
-            print("Введите количество 3:")
+            print("Введите количество 3: ")
             count3 = readln().toUInt()
         }
-        catch (e: Exception)
-        {
+        catch (e: Exception){
             println("Неверный формат данных")
         }
     }
+
     //Подсчет стипендии
     fun GetSchlrshp():Double{
         if (count3 == 0u && count4 == 0u && count5 != 0u)
-            scholarship == 1598.5
-        else if (count3 == 0u && count4>0u && count5>0u)
-            scholarship == 1066.05
+            scholarship = 1598.5
+        else if (count3 == 0u && count4 > 0u && count5 > 0u)
+            scholarship = 1066.05
         else
-            scholarship == 0.0
+            scholarship = 0.0
         return scholarship
     }
+    //Перегрузка функции вывода информации о студенте
+    override fun Info(): String {
+        if (correct)
+            return super.Info() + " Получает стипендию в размере $scholarship."
+        else
+            return ""
+    }
+
 }
